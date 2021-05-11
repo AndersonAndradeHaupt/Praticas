2dc2b128c3fa0c0b129949f9a549d47b914f74a1
FyodaHaupt <80731486+FyodaHaupt@users.noreply.github.com>
2021-05-11 18:58:29 -0400

    Add files via upload

---
+++ CotaçãoBitCoinJs/js/Index.js
@@ -0,0 +1,35 @@
+$.support.cors = true;
+
+$(document).ready(
+    function(){
+        ConsultaValor();
+    
+    }
+  
+)
+
+setInterval("ConsultaValor()", 1000);
+
+function ConsultaValor(){
+    $.ajax({
+        type:'GET',
+        url:'https://www.mercadobitcoin.net/api/BTC/ticker/',
+        datatype : 'json',
+        success: function(data){
+         var  vlr = data.ticker.last;
+
+         if (document.getElementById('valor').value > vlr) {
+            document.getElementById('valor').style.backgroundColor = '#006400';
+
+         } else {
+            document.getElementById('valor').style.backgroundColor = '#FF0000'; 
+         } 
+        document.getElementById('valor').value = vlr;
+
+             
+        },
+        error: function(data){
+            document.getElementById('valor').value = 'Sem conexão com a internet!';
+        }
+    })
+}
